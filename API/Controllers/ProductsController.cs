0af4e72cd0c3032d98b82191b7aeafc7cf1d8ffd
shkelqimii <96846815+shkelqimii@users.noreply.github.com>
2022-06-19 19:45:02 +0200

    Update ProductsController.cs

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
         public readonly StoreContext _context;
         public ProductsController(StoreContext context)
         {
-            _context = context;
+            _context = contexxt;
         }
 
         [HttpGet]
