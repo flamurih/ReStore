59c573e4fcb2a6bb981a977443b6f2d50de71ede
shkelqimii <96846815+shkelqimii@users.noreply.github.com>
2022-09-29 15:45:53 +0200

    Improvements in section 10

--- client/src/features/basket/BasketSummary.tsx
+++ client/src/features/basket/BasketSummary.tsx
@@ -2,22 +2,41 @@ import { Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@m
 import { useAppSelector } from '../../app/store/configureStore';
 import { currencyFormat } from '../../app/util/util';
 
-export default function BasketSummary() {
+interface Props {
+    subtotal?: number;
+}
+
+export default function BasketSummary({subtotal}: Props) {
     const {basket} = useAppSelector(state => state.basket);
-    const total = basket?.items.reduce((sum, item) => sum + (item.quantity * item.price),0) ?? 0;
+    if (subtotal === undefined)
+        subtotal = basket?.items.reduce((sum, item) => sum + (item.quantity * item.price),0) ?? 0;
+    const deliveryFee = subtotal > 10000 ? 0 : 500;
 
   return (
     <>
         <TableContainer component={Paper} variant={'outlined'}>
-            <Table sx={{display:'flex',justifyContent:'flex-end'}}>
-                <TableBody>
-                    <TableRow>
-                        <TableCell colSpan={4}><strong>Total</strong></TableCell>
-                        <TableCell align='right'><strong>{currencyFormat(total)}</strong></TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </TableContainer>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colSpan={2}>Subtotal</TableCell>
+                            <TableCell align="right">{currencyFormat(subtotal)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Delivery fee*</TableCell>
+                            <TableCell align="right">{currencyFormat(deliveryFee)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Total</TableCell>
+                            <TableCell align="right">{currencyFormat(subtotal + deliveryFee)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <span style={{fontStyle: 'italic'}}>*Orders over $100 qualify for free delivery</span>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
     
     </>
   )
